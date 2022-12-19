86dcd7c252a0353e07fa43aa30ba9681fd2bda16
CherryRey <112253067+CherryRey@users.noreply.github.com>
2022-12-19 16:48:14 +0100

    modulo.
    En este ejercicio aprendimos a crear módulos e importarlos desde un archivo python.

---
+++ calcular.py
@@ -0,0 +1,14 @@
+from calculadora import suma, resta
+from calculadora import multiplicacion as multi
+from calculadora import division as div
+
+
+def main():
+    add = suma(2,3,4,5,7)
+    substract = resta(10,4,5)
+    multiply = multi(2,3,2)
+    dividir = div(12,2,2)
+
+
+if __name__ == "__main__":
+    main()
